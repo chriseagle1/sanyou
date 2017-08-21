68a8c3d316e82def30281517366d14117466398a
chengm <chengm@mysoft.com.cn>
2017-08-21 11:45:31 +0800

    .

--- controllers/ajax/common/UploadFileController.php
+++ controllers/ajax/common/UploadFileController.php
@@ -6,6 +6,31 @@ use yii\web\Controller;
 
 class UploadFileController extends Controller {
     public function actionAjaxUploader() {
+        $file = $_FILES['uploadFileName'];
+        if (empty($file['size'])) {
+            $res= $this->ajax_response(0, '上传文件为空') ;
+            $res->format= 'html';
+        
+            return json_encode($res);
+        }
+        if ($file['error']) {
+            $res= $this->ajax_response(0, '上传文件出错') ;
+            $res->format= 'html';
+            return json_encode($res);
+        }
+        
+        $type = pathinfo($file['name'], PATHINFO_EXTENSION); //非严格意义的从原文件名上获取到的扩展名
+        $type = strtolower($type);
+        $validExt = ['xls','xlsx', 'csv'];
+        if (!in_array($type, $validExt)) {
+            $res= $this->ajax_response(0, '只支持EXCEL文件类型！') ;
+            $res->format= 'html';
+            return json_encode($res);
+        }
+        
+        $fileName = md5($file['name'] . '#' . date('YmdHis') . '_' . rand(10000, 99999)) . '.' . $type;
+        $filePath = \Yii::getAlias('@webroot') . '/attachs/' . $fileName;
+        move_uploaded_file($file['tmp_name'], $filePath);
         $type = 'xlsx';
         $file_path = '';
         
--- views/user/user-sign/index.php
+++ views/user/user-sign/index.php
@@ -1,1 +1,42 @@
-index.php
+
+<button class="btn btn-default btn-file" type="button">上传文件</button>
+
+<script>
+var importFile = new AjaxUpload('.btn-file', {
+    action: "url",
+    name: "uploadFileName",
+    data: {
+    	param1: value1
+    },
+    autoSubmit: false,
+    responseType: 'json',
+    onChange: function (file, extension) {
+        $('#file_name').val(file);
+        if (!new RegExp(/(xls)|(xlsx)/i).test(extension)) {
+        	$('#import_error').text('只支持EXCEL文件类型！');
+            $('#btn_import').prop('disabled', true);
+            return false;
+        } else {
+        	$('#import_error').text('');
+            $('#btn_import').prop('disabled', false);
+            return true;
+        }
+    },
+    onSubmit: function (file, extension) {
+    },
+    onComplete: function (file, response) {
+    	$('#file_name').val('');
+    	$('#btn_import').prop('disabled', true);
+
+        var result = response.data;
+        if (result.isSuccess) {
+        	
+
+        } else {
+        }
+    },
+    onTimeout: function () {
+        alert('上传超时，请重试。');
+    }
+});
+</script>
