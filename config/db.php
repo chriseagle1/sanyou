3c728555d0642fe806440f943e3cb12ba1f962c6
chengm <chengm@mysoft.com.cn>
2017-08-23 18:07:08 +0800

    .

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.5.103.35;dbname=world',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8',
-];
